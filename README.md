659683c10be09e9a4c7f1abfe78695229d73f3e8
Mixton <nastymax14@gmail.com>
2017-04-08 17:51:57 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # zabbix-oio
+
+DEBIAN
+wget https://github.com/Mixton/zabbix-oio/userparameter_openio.conf -O /etc/zabbix/zabbix_agentd.conf.d/userparameter_openio.conf
+wget https://github.com/Mixton/zabbix-oio/zabbix-oiosds-discover.py -O /usr/local/bin/zabbix-oiosds-discover.py
+chmod +x /usr/local/bin/zabbix-oiosds-discover.py
+systemctl restart zabbix-agent.service
+
+import template
+
+CENTOS/RedHat
+wget https://github.com/Mixton/zabbix-oio/userparameter_openio.conf -O /etc/zabbix_agentd.conf.d/userparameter_openio.conf
+wget https://github.com/Mixton/zabbix-oio/zabbix-oiosds-discover.py -O /usr/local/bin/zabbix-oiosds-discover.py
+chmod +x /usr/local/bin/zabbix-oiosds-discover.py
+systemctl restart zabbix-agent.service
+
+import template
---
+++ zabbix-oiosds-discover.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import os
+import subprocess
+import shlex
+import json
+import socket
+import netifaces
+
+if __name__ == "__main__":
+    #hostname = socket.gethostname()
+    ifaces = netifaces.interfaces()
+    ifacesbanned = ('lo', 'bond0')
+    ifaces = (iface for iface in ifaces if not any(ifacebanned in iface for ifacebanned in ifacesbanned))
+    for iface in ifaces:
+        addrs = netifaces.ifaddresses(iface)
+        break
+    hostname = addrs[netifaces.AF_INET][0]['addr']
+    cmd = '/usr/bin/oio-cluster -r OPENIO  '
+    #cmd = raw_input("shell:")
+    args = shlex.split(cmd)
+    output,error = subprocess.Popen(args,stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
+    #print error
+    output = output.split('\n')
+    servs = []
+    services = (service for service in output
+                if (hostname in service))
+    for service in services:
+        service = ' '.join(service.split('|'))
+        serv = service.split(' ')[1] + "-" + service.split(' ')[2]
+        #serv = ''.join(serv.split(':'))
+        servs.append(serv)
+    data = [{"{#SERVICENAME}": service} for service in servs]
+    print(json.dumps({"data": data}, indent=4))
