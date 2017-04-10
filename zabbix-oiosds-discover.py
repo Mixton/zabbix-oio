5842ea24d78be35c846c869ebcf72890bd5d7589
Mixton <nastymax14@gmail.com>
2017-04-10 22:47:06 +0200

    does not use oio-cluster anymore

--- zabbix-oiosds-discover.py
+++ zabbix-oiosds-discover.py
@@ -5,8 +5,28 @@ import shlex
 import json
 import socket
 import netifaces
+import requests
+import argparse
+import textwrap
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(prog='zabbix-oiosds-discover.py', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
+                        TODO
+                        '''), epilog="e.g. zabbix-oiosds-discover.py --ns <OIO-NS> -p <OIO-PROXY port> -s <service>")
+    parser.add_argument('--ns', help='set oio-ns', required=False, default='OPENIO')
+    parser.add_argument('-p', '--port', help='set oio-proxy port', required=False, default='6006')
+    parser.add_argument('-s', '--servicename', help='servicename = <service_type-service_id>', required=False)
+    parser.add_argument('--getscore', help='get score (need servicename to be set)', required=False, action='store_true')
+    parser.add_argument('--getstatus', help='get status (need servicename to be set)', required=False, action='store_true')
+    parser.add_argument('--version', action='version', version='%(prog)s v0.1')
+    args = parser.parse_args()
+
+    NS = args.ns
+    PROXY_PORT = args.port
+
+    services_list = ('account', 'rdir', 'meta0', 'meta1', 'meta2', 'rawx')
+
     #hostname = socket.gethostname()
     ifaces = netifaces.interfaces()
     ifacesbanned = ('lo')
@@ -15,19 +35,61 @@ if __name__ == "__main__":
         addrs = netifaces.ifaddresses(iface)
         break
     hostname = addrs[netifaces.AF_INET][0]['addr']
-    cmd = '/usr/bin/oio-cluster -r OPENIO  '
-    #cmd = raw_input("shell:")
-    args = shlex.split(cmd)
-    output,error = subprocess.Popen(args,stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
-    #print error
-    output = output.split('\n')
-    servs = []
-    services = (service for service in output
-                if (hostname in service))
-    for service in services:
-        service = ' '.join(service.split('|'))
-        serv = service.split(' ')[1] + "-" + service.split(' ')[2]
-        #serv = ''.join(serv.split(':'))
-        servs.append(serv)
-    data = [{"{#SERVICENAME}": service} for service in servs]
-    print(json.dumps({"data": data}, indent=4))
+
+    if args.servicename:
+        service = args.servicename.split('-')[0]
+        service_id = args.servicename.split('-')[1]
+        url = "http://%s:%s/v3.0/%s/conscience/list?type=%s"%(hostname, PROXY_PORT, NS, service)
+        try:
+            r = requests.get(url, verify=False)
+        except Exception, e:
+            pass
+
+        if r.status_code == 200:
+
+            if args.getscore:
+                score = 0
+                for host in r.json():
+                    if service_id in host['addr']:
+                        score = host['score']
+
+
+                print(score)
+
+            if args.getstatus:
+                active = False
+                for host in r.json():
+                    if service_id in host['addr']:
+                        active = host['tags']['tag.up']
+
+
+                print(active)
+
+        else:
+
+            if args.getscore:
+                print(0)
+
+            if args.getstatus:
+                print(False)
+
+    else:
+
+        services = []
+
+        for service in services_list:
+            url = "http://%s:%s/v3.0/%s/conscience/list?type=%s"%(hostname, PROXY_PORT, NS, service)
+            try:
+                r = requests.get(url, verify=False)
+            except Exception, e:
+                pass
+        
+            if r.status_code == 200:
+                for host in r.json():
+                    if hostname in host['addr']:
+                        serv = "%s-%s"%(service, host['addr'])
+                        services.append(serv)
+
+        if services:
+            data = [{"{#SERVICENAME}": service} for service in services]
+            print(json.dumps({"data": data}, indent=4))
